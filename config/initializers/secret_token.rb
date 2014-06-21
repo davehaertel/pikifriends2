500e18702d3cd879876f001be3cfc3dbeb4096e1
Clay Stewart <claytonmstewart@gmail.com>
2014-06-21 12:59:40 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pikifriends::Application.config.secret_key_base = '5c0f9a74bed0e3ad473092857a7b2659b95cc411d6d7a93fec260d01959ec41e7b8026ec11a1967a01e5f945452c9b3099812692d468089d286232d670c996f7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pikifriends::Application.config.session_store :cookie_store, key: '_pikifriends_session'
